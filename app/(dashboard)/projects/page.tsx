9f1cc1f138388bc9e6ac9de27db9caca116f13de
jasser ben jomaa <jasserj663@gmail.com>
2026-07-20 01:49:26 +0100

    starting with CRUD projects

--- app/(dashboard)/projects/page.tsx
+++ app/(dashboard)/projects/page.tsx
@@ -1,20 +1,6 @@
 import { Project } from "@/features/projects-columns";
 import { ProjectsView } from "@/components/projects-view";
-const MOCK_PROJETS: Project[] = [
-  {
-    id: "1",
-    name: "Website Redesign",
-    status: "in_progress",
-    dueDate: "2026-08-15",
-  },
-  { id: "2", name: "Mobile App", status: "not_started", dueDate: "2026-09-01" },
-  {
-    id: "3",
-    name: "Data Migration",
-    status: "completed",
-    dueDate: "2026-06-30",
-  },
-];
+
 const ProjectsPage = () => {
   return (
     <main>
@@ -25,7 +11,7 @@ const ProjectsPage = () => {
             Browse, search, and manage all your projects in one place.
           </p>
         </div>
-        <ProjectsView projects={MOCK_PROJETS} />
+        <ProjectsView projects={[]} />
       </div>
     </main>
   );
